f6071a792ad033111ab94d06c8c31db29baabc10
tulexx <marcin.tula@gmail.com>
2020-08-22 23:29:09 +0200

    added i3

---
+++ .config/monitors/__Work.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --mode 2560x1440 --pos 0x0 --rotate normal --output DVI-I-1-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DVI-I-2-2 --off
